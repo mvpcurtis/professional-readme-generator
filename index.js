34b6906a9e2565ba12b708b60f9ae49995a2f72d
mvpcurtis <curtis.bowen@gmx.com>
2024-02-05 21:08:49 +0000

    inital commit, testing inquirer

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
-
-// array of questions for user
+const inquirer = require('inquirer');
 const questions = [
-
+  {
+    type: 'input',
+    name: 'input test',
+    message: 'What is your name?',
+    validate: (answer) => {
+      if (answer === '') {
+        return 'Please enter a valid name';
+      }
+      return true;
+    },
+  },
+  {
+    type: 'list',
+    name: 'list test',
+    message: 'How many dogs do you have?',
+    choices: [1, 3, 6, 8],
+  },
+  {
+    type: 'checkbox',
+    name: 'checkbox test',
+    message: 'How many programming languages do you know?',
+    choices: ['JS', 'C++', 'Ruby', 'Python'],
+  },
 ];
+console.log(`
+--------------------------------------
+---- PROFESSIOAL README GENERATOR ----
+--------------------------------------
+`);
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
